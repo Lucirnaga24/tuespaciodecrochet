6dff4b61a3bee34e65964b2223b523aa76eaaf77
Lucirnaga24 <lucreciaartusi02@gmail.com>
2025-03-15 15:33:49 -0300

    agrego seccion contacto / form

---
+++ contacto/js/form.js
@@ -0,0 +1,62 @@
+// VALIDACION DEL FORMULARIO DE CONTACTO
+let contactForm = document.querySelector('#contactForm');
+let nameInput = document.querySelector('#name');
+let nameValidation = document.querySelector('#nameValidation');
+let emailInput = document.querySelector('#email');
+let emailValidation = document.querySelector('#emailValidation');
+let subjectInput = document.querySelector('#subject');
+let subjectValidation = document.querySelector('#subjectValidation');
+let messageInput = document.querySelector('#message');
+let messageValidation = document.querySelector('#messageValidation');
+
+const validateInput = (input, validationElement, validationFunction) => {
+    if (!validationFunction(input.value.trim())) {
+        validationElement.classList.remove('hidden');
+        return false;
+    } else {
+        validationElement.classList.add('hidden');
+        return true;
+    }
+};
+
+const validateName = () =>   validateInput(nameInput, nameValidation, (name) => 
+    /^[a-zA-ZÀ-ÿ\s]+$/.test(name.trim()) && name.trim() !== ''
+  );
+const validateEmail = () => validateInput(emailInput, emailValidation, (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email));
+const validateSubject = () => validateInput(subjectInput, subjectValidation, (subject) => subject !== '');
+const validateMessage = () => validateInput(messageInput, messageValidation, (message) => message !== '');
+
+const validateForm = () => {
+    return validateName() && validateEmail() && validateSubject() && validateMessage();
+};
+
+contactForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    if (validateForm()) {
+        alert('Form submitted successfully!');
+        this.submit();
+    }
+});
+
+const inputs = [nameInput, emailInput, subjectInput, messageInput];
+inputs.forEach(input => {
+    input.addEventListener('input', function () {
+        switch (input.id) {
+            case 'name':
+                validateName();
+                break;
+            case 'email':
+                validateEmail();
+                break;
+            case 'subject':
+                validateSubject();
+                break;
+            case 'message':
+                validateMessage();
+                break;
+            default:
+                break;
+        }
+    });
+});
